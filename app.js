0a8738f649d583450fc5f9c123f3be7159d09df5
Sasha Kolomiytsev <kolomiytsevs@googlemail.com>
2022-09-24 13:17:16 +0100

    fix: add cors

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(compression())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(express.json()) 
+app.use(cors)
 
 app.use((req, res, next)=>{
   res.header('Access-Control-Allow-Origin', '*')
